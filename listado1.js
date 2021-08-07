9f81d7c9443a3c17e4d7fc3bdd0094a0fec12c25
yenia-m <88461546+yenia-m@users.noreply.github.com>
2021-08-07 04:06:24 -0300

    Update listado1.js

--- listado1.js
+++ listado1.js
@@ -1,4 +1,4 @@
-const goleadores_url= "https:/nicobolso.github.io/goleadores/listado.json"
+const goleadores_url= "https://yenia-m.github.io/equipo/listado.json"
 
 function cargandoDatos(url){
     document.getElementById("data").innerHTML="";
